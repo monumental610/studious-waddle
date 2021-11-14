bdd5048ad4ecc83199d5160c517e3311161adca9
monumental610 <dusng607@gmail.com>
2021-11-14 13:26:34 +0530

    get price binance

--- coinpairgiver.py
+++ coinpairgiver.py
@@ -1,36 +1,37 @@
 import requests # to get data from api
 import json # to convert json file to dict
-
-def Stringinize(string):     #seperate string data into seperated list data
-    li = list(string.split(" "))
-    return li
+import time # to see performance time
 
 response = requests.get("https://api.wazirx.com/api/v2/market-status") # gets data from wazirx in 'response' type data
 #print(response.status_code)
 data = json.loads(response.text) #converts 'response' type data to python 'dict'
 #print(type(data))
 #print(data['assets']) # part which is under use
-deposit = None
-withdrawal = None
 
 for asset in data['assets']:
     if asset['deposit'] == 'enabled':
         deposit = asset['type'] #prints codename of the coin ada,btc etc
-        #depositstring = Stringinize(deposit)
-        #print(type(depositstring))     #prints coins which have deposit enabled in seperated list form
-        symboldeposit = deposit + "USDT"
-        depositsymbol = symboldeposit.upper() 
-        print(depositsymbol) #prints out coin pairs to be given to binance in uppercase letter
-        
-
+        symboldeposit = "https://api.binance.com/api/v3/ticker/price?symbol=" + deposit.upper() + "USDT"
+        #print(symboldeposit) #prints out coin pair links to be given to binance 
+        responsedepositsymbolprice = requests.get(symboldeposit) #GETS symbol price
+        #print(responsedepositsymbolprice.status_code)
+        depositsymbolprice = json.loads(responsedepositsymbolprice.text)
+        #print(type(depositsymbolprice))
+        #print(depositsymbolprice)
+        if responsedepositsymbolprice.status_code == 200 : #if symbolpair is correct goes to next step
+            print(depositsymbolprice)
+print("yes")
 for asset in data['assets']:
     if asset['withdrawal'] == 'enabled':
         withdrawal = asset['type'] #prints codename of the coin ada,btc etc
-        #withdrawalstring = Stringinize(withdrawal) 
-        #print(type(withdrawalstring)) #prints coins which have withdrawal enabled in seperated list form
-        symbolwithdrawal = withdrawal + "USDT"
-        withdrawalsymbol = symbolwithdrawal.upper()
-        print(withdrawalsymbol) #prints out coin pairs to be given to wazirx in uppercase letter
-       
+        symbolwithdrawal = "https://api.wazirx.com/uapi/v1/ticker/24hr?symbol=" + withdrawal + "usdt"
+        #print(symbolwithdrawal) #prints out coin pair links to be given to binance 
+        responsewithdrawalsymbolprice = requests.get(symbolwithdrawal) #GETS symbol price
+        #print(responsewithdrawalsymbolprice.status_code)
+        withdrawalsymbolprice = json.loads(responsewithdrawalsymbolprice.text)
+        #print(type(withdrawalsymbolprice))
+        #print(withdrawalsymbolprice)
+        if responsewithdrawalsymbolprice.status_code == 200 : #if symbolpair is correct goes to next step
+            print(withdrawalsymbolprice['lastPrice'])
 
-#symbolprice = requests.get("https://api.binance.com/api/v3/ticker/price?symbol=ADAUSDT")
+print(time.process_time()) # to see code performance time
